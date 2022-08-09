a5b436c8058ba68bd7ce62b8d6aafeb5170cfcdb
Kim, Choong Hun <32251385+zennken@users.noreply.github.com>
2022-08-09 11:17:28 +0900

    Remove useless property files.

---
+++ plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/AltibaseMessages.java
@@ -0,0 +1,7 @@
+package org.jkiss.dbeaver.ext.altibase;
+
+import org.jkiss.dbeaver.ext.altibase.util.StringUtil;
+
+public class AltibaseMessages {
+	public static final String NO_DBMS_METADATA = "-- Without DBMS_METADATA package, the generated DDL may not be accurate." + StringUtil.NEW_LINE;
+}
