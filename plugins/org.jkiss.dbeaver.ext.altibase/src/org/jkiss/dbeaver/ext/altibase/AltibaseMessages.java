a58de84670b14d585c0fb94f07e8e77405d2ea6f
Kim, Choong Hun <32251385+zennken@users.noreply.github.com>
2022-09-01 14:48:45 +0900

    Remove useless old plugin

--- plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/AltibaseMessages.java
+++
@@ -1,7 +0,0 @@
-package org.jkiss.dbeaver.ext.altibase;
-
-import org.jkiss.dbeaver.ext.altibase.util.StringUtil;
-
-public class AltibaseMessages {
-	public static final String NO_DBMS_METADATA = "-- Without DBMS_METADATA package, the generated DDL may not be accurate." + StringUtil.NEW_LINE;
-}
