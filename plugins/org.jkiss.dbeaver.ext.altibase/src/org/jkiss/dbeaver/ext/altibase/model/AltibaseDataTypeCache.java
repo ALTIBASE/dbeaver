71f3d2f01d7c358cb9d78fd1c750c67fd1381622
Kim, Choong Hun <32251385+zennken@users.noreply.github.com>
2022-09-05 14:34:40 +0900

    DB2. Data type: NCHAR length

--- plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/model/AltibaseDataTypeCache.java
+++ plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/model/AltibaseDataTypeCache.java
@@ -1,33 +1,28 @@
-/*
- * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2022 DBeaver Corp and others
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.jkiss.dbeaver.ext.altibase.model;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jkiss.code.NotNull;
+import org.jkiss.dbeaver.DBException;
 import org.jkiss.dbeaver.Log;
 import org.jkiss.dbeaver.ext.generic.model.GenericDataTypeCache;
 import org.jkiss.dbeaver.ext.generic.model.GenericStructContainer;
+import org.jkiss.dbeaver.model.DBUtils;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCPreparedStatement;
 import org.jkiss.dbeaver.model.exec.jdbc.JDBCResultSet;
+import org.jkiss.dbeaver.model.exec.jdbc.JDBCSession;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCConstants;
 import org.jkiss.dbeaver.model.impl.jdbc.JDBCUtils;
+import org.jkiss.dbeaver.model.impl.jdbc.cache.JDBCBasicDataTypeCache;
+import org.jkiss.dbeaver.model.messages.ModelMessages;
+import org.jkiss.dbeaver.model.runtime.DBRProgressMonitor;
 
 /**
- * GenericDataTypeCache
+ * AltibaseDataTypeCache
  */
-public class AltibaseDataTypeCache extends GenericDataTypeCache
+public class AltibaseDataTypeCache extends JDBCBasicDataTypeCache<GenericStructContainer, AltibaseDataType>
 {
     private static final Log log = Log.getLog(GenericDataTypeCache.class);
 
@@ -35,17 +30,74 @@ public class AltibaseDataTypeCache extends GenericDataTypeCache
         super(owner);
     }
 
-    protected AltibaseDataType makeDataType(@NotNull JDBCResultSet dbResult, String name, int valueType) {
-        return new AltibaseDataType(
-                owner,
-                valueType,
-                name,
-                JDBCUtils.safeGetString(dbResult, JDBCConstants.LOCAL_TYPE_NAME),
-                JDBCUtils.safeGetBoolean(dbResult, JDBCConstants.UNSIGNED_ATTRIBUTE),
-                JDBCUtils.safeGetInt(dbResult, JDBCConstants.SEARCHABLE) != 0,
-                JDBCUtils.safeGetInt(dbResult, JDBCConstants.PRECISION),
-                JDBCUtils.safeGetInt(dbResult, JDBCConstants.MINIMUM_SCALE),
-                JDBCUtils.safeGetInt(dbResult, JDBCConstants.MAXIMUM_SCALE));
+    @Override
+    protected synchronized void loadObjects(DBRProgressMonitor monitor, GenericStructContainer container) throws DBException {
+        AltibaseDataSource dataSource = (AltibaseDataSource) container.getDataSource();
+
+        if (dataSource == null) {
+            throw new DBException(ModelMessages.error_not_connected_to_database);
+        }
+        
+        // Load domain types
+        List<AltibaseDataType> tmpObjectList = new ArrayList<>();
+
+        /*
+        for (AltibaseFieldType fieldType : AltibaseFieldType.values()) {
+            AltibaseDataType dataType = new AltibaseDataType(dataSource, fieldType);
+            tmpObjectList.add(dataType);
+        }
+        */
+
+        try {
+            try (JDBCSession session = DBUtils.openMetaSession(monitor, dataSource, "Load Altibase data types")) {
+                // Use CAST to improve performance, binaries are too slow
+                try (JDBCPreparedStatement dbStat = session.prepareStatement(
+                    "SELECT * FROM V$DATATYPE ORDER BY TYPE_NAME"))
+                {
+                    monitor.subTask("Load Altibase domain types");
+                    try (JDBCResultSet dbResult = dbStat.executeQuery()) {
+                        while (dbResult.next()) {
+                            if (monitor.isCanceled()) {
+                                break;
+                            }
+                            // super(owner, valueType, name, remarks, unsigned, searchable, precision, minScale, maxScale);
+                            String typeName = JDBCUtils.safeGetString(dbResult, "TYPE_NAME");
+                            if (typeName == null) {
+                                continue;
+                            }
+                            int dataTypeId = JDBCUtils.safeGetInt(dbResult, "DATA_TYPE");
+                            boolean searchabel = (JDBCUtils.safeGetInt(dbResult, "SEARCHABLE") > 0);
+                            int precision = JDBCUtils.safeGetInt(dbResult, "COLUMN_SIZE");
+                            int nullable = JDBCUtils.safeGetInt(dbResult, "NULLABLE");
+                            boolean unsinged = (JDBCUtils.safeGetInt(dbResult, "UNSIGNED_ATTRIBUTE") == 1);
+                            String remarks = JDBCUtils.safeGetString(dbResult, "LOCAL_TYPE_NAME");
+                            int minScale = JDBCUtils.safeGetInt(dbResult, "MINIMUM_SCALE");
+                            int maxScale = JDBCUtils.safeGetInt(dbResult, "MAXIMUM_SCALE");
+
+                            AltibaseFieldType fieldType = AltibaseFieldType.getById(dataTypeId);
+                            if (fieldType == null) {
+                                log.error("Field type '" + fieldType + "' not found");
+                                continue;
+                            }
+
+                            // GenericStructContainer owner, int valueType, String name, String remarks, boolean unsigned,
+                			//boolean searchable, int precision, int minScale, int maxScale) {
+                            AltibaseDataType dataType = new AltibaseDataType(
+                                dataSource, fieldType, typeName, remarks, unsinged, searchabel, precision, minScale, maxScale);
+                            tmpObjectList.add(dataType);
+                        }
+                    }
+                }
+            } catch (SQLException ex) {
+                throw new DBException(ex, dataSource);
+            }
+        } catch (DBException e) {
+            if (!handleCacheReadError(e)) {
+                throw e;
+            }
+        }
+
+        mergeCache(tmpObjectList);
     }
 
 }
--- plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/model/AltibaseFieldType.java
+++ plugins/org.jkiss.dbeaver.ext.altibase/src/org/jkiss/dbeaver/ext/altibase/model/AltibaseFieldType.java
@@ -1,19 +1,3 @@
-/*
- * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2022 DBeaver Corp and others
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.jkiss.dbeaver.ext.altibase.model;
 
 import org.jkiss.dbeaver.model.DBPDataKind;
@@ -21,31 +5,51 @@ import org.jkiss.dbeaver.model.DBPDataKind;
 import java.sql.Types;
 
 public enum AltibaseFieldType {
-	SMALLINT(7, Types.SMALLINT, DBPDataKind.NUMERIC, "SMALLINT"),
-	INTEGER(8, Types.INTEGER, DBPDataKind.NUMERIC, "INTEGER"),
-	FLOAT(10, Types.FLOAT, DBPDataKind.NUMERIC, "FLOAT"),
-	DATE(12, Types.DATE, DBPDataKind.DATETIME, "DATE"),
-	TIME(13, Types.TIME, DBPDataKind.DATETIME, "TIME"),
-	CHAR(14, Types.CHAR, DBPDataKind.STRING, "CHAR"),
-	BIGINT(16, Types.BIGINT, DBPDataKind.NUMERIC, "BIGINT"),
-    NUMERIC(16, Types.NUMERIC, DBPDataKind.NUMERIC, "NUMERIC"), // Equal id, but another subtype - 1
-    DECIMAL(16, Types.DECIMAL, DBPDataKind.NUMERIC, "DECIMAL"), // Equal id, but another subtype - 2
-    BOOLEAN(23, Types.BOOLEAN, DBPDataKind.BOOLEAN, "BOOLEAN"),
-	DOUBLE_PRECISION(27, Types.DOUBLE, DBPDataKind.NUMERIC, "DOUBLE PRECISION"),
-	TIMESTAMP(35, Types.TIMESTAMP, DBPDataKind.DATETIME, "TIMESTAMP"),
-	VARCHAR(37, Types.VARCHAR, DBPDataKind.STRING, "VARCHAR"),
-	BLOB(261, Types.BLOB, DBPDataKind.CONTENT, "BLOB");
+	// char types
+	CHAR 	("CHAR", 	1,  DBPDataKind.STRING, Types.CHAR),
+	VARCHAR ("VARCHAR", 12, DBPDataKind.STRING, Types.VARCHAR),
+	NCHAR  	("NCHAR", 	-8, DBPDataKind.STRING, Types.CHAR), // Types.NCHAR returns question mark.
+	NVARCHAR("NVARCHAR",-9, DBPDataKind.STRING, Types.NVARCHAR),
+	// encrypted column data type
+	ECHAR	("ECHAR",	60, DBPDataKind.STRING, Types.BINARY),
+	EVARCHAR("EVARCHAR",61, DBPDataKind.STRING, Types.BINARY),
 
+	// number types
+	INTEGER	("INTEGER",  4,  	DBPDataKind.NUMERIC, Types.INTEGER),
+	SMALLINT("SMALLINT", 5,  	DBPDataKind.NUMERIC, Types.SMALLINT),
+	BIGINT 	("BIGINT", 	 -5, 	DBPDataKind.NUMERIC, Types.BIGINT),
+	REAL 	("REAL", 	 7,  	DBPDataKind.NUMERIC, Types.REAL),
+	NUMBER	("NUMBER", 	 10002, DBPDataKind.NUMERIC, Types.NUMERIC),
+	NUMERIC ("NUMERIC",  2, 	DBPDataKind.NUMERIC, Types.NUMERIC),
+	DOUBLE 	("DOUBLE", 	 8, 	DBPDataKind.NUMERIC, Types.DOUBLE),
+	FLOAT 	("FLOAT", 	 6, 	DBPDataKind.NUMERIC, Types.FLOAT),
+
+	// date & time
+	DATE 	("DATE", 	 9,  	DBPDataKind.DATETIME, Types.TIMESTAMP),
+
+	// binary
+	BIT 	("BIT", 	 -7, 	DBPDataKind.BINARY, Types.BINARY),
+	VARBIT 	("VARBIT", 	 -100, 	DBPDataKind.BINARY, Types.BINARY),
+	BYTE 	("BYTE", 	 20001, DBPDataKind.BINARY, Types.BINARY),
+	VARBYTE ("VARBYTE",  20003, DBPDataKind.BINARY, Types.BINARY),
+	NIBBLE 	("NIBBLE", 	 20002, DBPDataKind.BINARY, Types.BINARY),
+	BINARY 	("BINARY", 	 -2, 	DBPDataKind.BINARY, Types.BINARY),
+
+	CLOB 	("CLOB", 	 40, 	DBPDataKind.BINARY, Types.CLOB),
+	BLOB 	("BLOB", 	 30, 	DBPDataKind.BINARY, Types.BLOB),
+	GEOMETRY("GEOMETRY", 10003, DBPDataKind.BINARY, Types.BLOB);
+
+    private final String name;
     private final int typeID;
     private final int valueType;
     private final DBPDataKind dataKind;
-    private final String name;
+    
 
-    AltibaseFieldType(int typeID, int valueType, DBPDataKind dataKind, String name) {
-        this.typeID = typeID;
+    AltibaseFieldType(String name, int typeID, DBPDataKind dataKind, int valueType) {
+    	this.name = name;
+    	this.typeID = typeID;
         this.valueType = valueType;
         this.dataKind = dataKind;
-        this.name = name;
     }
 
     public int getTypeID() {
@@ -64,17 +68,8 @@ public enum AltibaseFieldType {
         return name;
     }
 
-    public static AltibaseFieldType getById(int id, int subTypeId) {
-        if (id == 16) {
-            switch (subTypeId) {
-                case 1:
-                    return NUMERIC;
-                case 2:
-                    return DECIMAL;
-                default:
-                    return BIGINT;
-            }
-        }
+    public static AltibaseFieldType getById(int id) {
+
         for (AltibaseFieldType ft : values()) {
             if (ft.getTypeID() == id) {
                 return ft;
